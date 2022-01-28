efe4e654039f024768b346ddcb68e35b4e837ac8
Andy Goldstein <andy.goldstein@redhat.com>
2022-01-28 14:29:25 -0500

    Revert "HACK: let user set cert, key, token for loopback"
    This reverts commit f374c6c7f6147a88a1c25cdabc4192d502c26be8.

--- staging/src/k8s.io/apiserver/pkg/server/options/serving_with_loopback.go
+++ staging/src/k8s.io/apiserver/pkg/server/options/serving_with_loopback.go
@@ -17,10 +17,7 @@ limitations under the License.
 package options
 
 import (
-	"errors"
 	"fmt"
-	"net"
-	"strconv"
 
 	"github.com/google/uuid"
 
@@ -32,30 +29,10 @@ import (
 
 type SecureServingOptionsWithLoopback struct {
 	*SecureServingOptions
-
-	// LoopbackOptions contains authentication and authorization options for the loopback client
-	// configuration.
-	LoopbackOptions *LoopbackOptions
-}
-
-// LoopbackOptions contains authentication and authorization options for the loopback client
-// configuration.
-type LoopbackOptions struct {
-	// Cert is the self-signed certificate. If unset, will be generated when ApplyTo is
-	// called.
-	Cert []byte
-	// Key is the private key for the self-signed certificate. If unset, will be
-	// generated when ApplyTo is called.
-	Key []byte
-	// Token is the bearer token the loopback client uses to communicate with the server. if
-	// unset, will be generated when ApplyTo is called.
-	Token string
 }
 
 func (o *SecureServingOptions) WithLoopback() *SecureServingOptionsWithLoopback {
-	return &SecureServingOptionsWithLoopback{
-		SecureServingOptions: o,
-	}
+	return &SecureServingOptionsWithLoopback{o}
 }
 
 // ApplyTo fills up serving information in the server configuration.
@@ -72,37 +49,12 @@ func (s *SecureServingOptionsWithLoopback) ApplyTo(secureServingInfo **server.Se
 		return nil
 	}
 
-	var (
-		certPem, keyPem []byte
-		loopbackToken   string
-		err             error
-	)
-
-	if s.LoopbackOptions != nil {
-		if len(s.LoopbackOptions.Cert) == 0 {
-			return errors.New("when specifying SecureServingOptionsWithLoopback.LoopbackOptions, Cert is required")
-		}
-		if len(s.LoopbackOptions.Key) == 0 {
-			return errors.New("when specifying SecureServingOptionsWithLoopback.LoopbackOptions, Key is required")
-		}
-		if s.LoopbackOptions.Token == "" {
-			return errors.New("when specifying SecureServingOptionsWithLoopback.LoopbackOptions, Token is required")
-		}
-
-		certPem = s.LoopbackOptions.Cert
-		keyPem = s.LoopbackOptions.Key
-		loopbackToken = s.LoopbackOptions.Token
-	} else {
-		// create self-signed cert+key with the fake server.LoopbackClientServerNameOverride and
-		// let the server return it when the loopback client connects.
-		certPem, keyPem, err = s.NewLoopbackClientCert()
-		if err != nil {
-			return fmt.Errorf("failed to generate self-signed certificate for loopback connection: %v", err)
-		}
-
-		loopbackToken = uuid.New().String()
+	// create self-signed cert+key with the fake server.LoopbackClientServerNameOverride and
+	// let the server return it when the loopback client connects.
+	certPem, keyPem, err := certutil.GenerateSelfSignedCertKey(server.LoopbackClientServerNameOverride, nil, nil)
+	if err != nil {
+		return fmt.Errorf("failed to generate self-signed certificate for loopback connection: %v", err)
 	}
-
 	certProvider, err := dynamiccertificates.NewStaticSNICertKeyContent("self-signed loopback", certPem, keyPem, server.LoopbackClientServerNameOverride)
 	if err != nil {
 		return fmt.Errorf("failed to generate self-signed certificate for loopback connection: %v", err)
@@ -111,7 +63,7 @@ func (s *SecureServingOptionsWithLoopback) ApplyTo(secureServingInfo **server.Se
 	// Write to the front of SNICerts so that this overrides any other certs with the same name
 	(*secureServingInfo).SNICerts = append([]dynamiccertificates.SNICertKeyContentProvider{certProvider}, (*secureServingInfo).SNICerts...)
 
-	secureLoopbackClientConfig, err := (*secureServingInfo).NewLoopbackClientConfig(loopbackToken, certPem)
+	secureLoopbackClientConfig, err := (*secureServingInfo).NewLoopbackClientConfig(uuid.New().String(), certPem)
 	switch {
 	// if we failed and there's no fallback loopback client config, we need to fail
 	case err != nil && *loopbackClientConfig == nil:
@@ -126,42 +78,4 @@ func (s *SecureServingOptionsWithLoopback) ApplyTo(secureServingInfo **server.Se
 	}
 
 	return nil
-}
-
-// NewLoopbackClientCert creates a self-signed certificate and key for the loopback client.
-func (s *SecureServingOptionsWithLoopback) NewLoopbackClientCert() ([]byte, []byte, error) {
-	certPem, keyPem, err := certutil.GenerateSelfSignedCertKey(server.LoopbackClientServerNameOverride, nil, nil)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to generate self-signed certificate for loopback connection: %v", err)
-	}
-	return certPem, keyPem, nil
-}
-
-// NewLoopbackClientConfig creates a loopback client *rest.Config for the given token and
-// certificate. API server composers can use this to create a loopback client configuration before
-// the server has started. It is slightly different from SecureServingInfo.NewLoopbackClientConfig
-// in that this method does not support a BindPort of 0, and it returns an error in that case.
-func (s *SecureServingOptionsWithLoopback) NewLoopbackClientConfig(token string, cert []byte) (*rest.Config, error) {
-	if s.BindPort == 0 {
-		return nil, errors.New("BindPort must be > 0")
-	}
-
-	hostAndPort := s.BindAddress.String() + ":" + strconv.Itoa(s.BindPort)
-	host, port, err := server.LoopbackHostPort(hostAndPort)
-	if err != nil {
-		return nil, err
-	}
-
-	c := &rest.Config{
-		// Do not limit loopback client QPS.
-		QPS:  -1,
-		Host: "https://" + net.JoinHostPort(host, port),
-		TLSClientConfig: rest.TLSClientConfig{
-			CAData:     cert,
-			ServerName: server.LoopbackClientServerNameOverride,
-		},
-		BearerToken: token,
-	}
-
-	return c, nil
 }
