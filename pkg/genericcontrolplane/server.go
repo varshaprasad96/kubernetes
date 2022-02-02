42bb0f5d02c9578381c6eb56d5a3ed4e6244fb1c
Sergiusz Urbaniak <sergiusz.urbaniak@gmail.com>
2022-02-02 15:17:14 +0100

    enable multicluster for rbac resources
    (cherry picked from commit 515bb638d62dcfeab4b723d581b3fdc6228dddad)

--- pkg/genericcontrolplane/server.go
+++ pkg/genericcontrolplane/server.go
@@ -313,7 +313,7 @@ func BuildGenericConfig(
 
 	kubeClientConfig := genericConfig.LoopbackClientConfig
 
-	clientutils.EnableMultiCluster(genericConfig.LoopbackClientConfig, genericConfig, true, "namespaces", "apiservices", "customresourcedefinitions")
+	clientutils.EnableMultiCluster(genericConfig.LoopbackClientConfig, genericConfig, true, "namespaces", "apiservices", "customresourcedefinitions", "clusterroles", "clusterrolebindings", "roles", "rolebindings")
 
 	clientgoExternalClient, err := clientgoclientset.NewForConfig(kubeClientConfig)
 	if err != nil {
